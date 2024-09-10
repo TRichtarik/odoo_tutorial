1b8cba2f39d774eb59a843d4cd94ef9c9a2c95a2
tomas <richtom21@gmail.com>
2024-09-11 00:14:35 +0200

    feat: create new real estate module (chapter 2)

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Tomas Richtarik",
+    'category': 'Real Estate',
+    'description': """
+        A module to manage real estate properties and offers.
+    """,
+    'installable': True,
+    'application': True,
+}
