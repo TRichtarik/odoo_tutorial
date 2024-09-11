1003265974e7a40380817d60c63d7feb53417b91
tomas <richtom21@gmail.com>
2024-09-11 19:58:38 +0200

    feat: actions, menus, fields, attributes and view (chapter 5)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
     'installable': True,
     'application': True,
     'data': [
-    'security/ir.model.access.csv',
-],
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml', ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
 from odoo import fields, models
 
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
     name = fields.Char(string="Property Name", required=True)
     description = fields.Text(string="Description")
+    active = fields.Boolean(string="Active", default=True)
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
-    expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    date_availability = fields.Date(string="Available From", copy=False,
+                                    default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -22,3 +24,10 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ], string="Garden Orientation")
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], string="State", required=True, copy=False, default='new')
